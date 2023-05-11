d4229ca6e91589c4cd8353916f307d17325ae314
LOH KEN HAW <LOH KEN HAW>
2023-05-11 12:03:10 +0800

    testing #2

--- constants/constants.js
+++ constants/constants.js
@@ -4,4 +4,3 @@ export const URL = "http://mpma-prod.us-east-1.elasticbeanstalk.com";
 export const ROLE_APPLICANT = "APPLICANT";
 export const ROLE_EMPLOYER = "EMPLOYER";
 export const ROLE_ADMIN = "ADMIN";
-export const ROLE_TEST = "TEST";
