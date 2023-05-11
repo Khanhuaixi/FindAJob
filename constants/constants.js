7f802c4e72c89afd6025c28045fb21d537e354cf
LOH KEN HAW <LOH KEN HAW>
2023-05-11 11:20:41 +0800

    Loh Ken Haw 11/5/2023 11:13am

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
-// export const URL = "http://mpma-dev.ap-southeast-1.elasticbeanstalk.com";
+// export const URL = "http://mpma-prod.us-east-1.elasticbeanstalk.com/";
 export const URL = "http://week8-dev.us-east-1.elasticbeanstalk.com";
 export const ROLE_APPLICANT = "APPLICANT";
 export const ROLE_EMPLOYER = "EMPLOYER";
