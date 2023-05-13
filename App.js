c8e775d0eb2ed015aba04e77218760c8eeacd50b
Khanhuaixi <khanhuaixi@gmail.com>
2023-05-13 23:50:03 +0800

    import employer job list and remove unused

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ import AdminProfile from "./src/features/admin/screens/AdminProfile";
 import { EmployerTabBar } from "./src/features/employer/navigation/EmployerTabBar";
 import EmployerProfile from "./src/features/employer/screens/EmployerProfile";
 import AdminApplicantManagement from "./src/features/admin/screens/AdminApplicantManagement";
+import EmployerJobList from "./src/features/employer/screens/EmployerJobList";
 
 const theme = {
   ...DefaultTheme,
